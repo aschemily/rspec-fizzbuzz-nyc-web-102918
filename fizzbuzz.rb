5e8cb12983965c75e2f3071de9df0e4a48e4c230
Emily Asch <e.asch@me.com>
2018-10-08 23:01:31 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(int)
-  if
+def fizzbuzz(number)
+  if number % 3
+    return "fizz"
+  elsif number % 5
+  return "buzz"
+  elsif (number % 3) && (number % 5)
+  return "fizzbuzz"
+else number % 3 || !(number % 5)
+  return "fizzbuzz"
   end
 end
