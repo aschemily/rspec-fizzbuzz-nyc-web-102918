ddc5e5458a827431986615b5d9ac8fec0cccd3a2
Emily Asch <e.asch@me.com>
2018-10-08 23:16:11 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,6 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
   if (number % 3 == 0) && (number % 5 == 0)
-    
     return "FizzBuzz"
     elsif number % 5 == 0 
     return "Buzz"
